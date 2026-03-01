a10d4342a109410e2baf38a7cc76c6f10838629d
Bandaru Srikanth <bandarusrikanth79@Bandarus-MacBook-Air.local>
2026-03-01 15:22:35 +0530

    whatsaap Link Fix

--- src/common/Info.js
+++ src/common/Info.js
@@ -1,7 +1,7 @@
 export const personal_info={
     mobile:"+91 9000160490",
     email:"eustpm@gmail.com",
-    whatsappLink:"https://wa.me/9000160490",
+    whatsappLink:"https://wa.me/+919000160490",
     twitterLink:"https://x.com/eushettyspeaker",
     instagramLink:"https://www.instagram.com/eushetty_official",
     youtubeChannel:"https://www.youtube.com/@eushettyspeaker",
